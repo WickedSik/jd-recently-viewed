521ff1b85ae5f3a58df884c3d9bb990c997136a9
Jurriën <jurriendokter@gmail.com>
2019-08-06 13:01:48 +0200

    Add time

--- lv-repo.php
+++ lv-repo.php
@@ -6,9 +6,9 @@ class jrv_lastviewed_repo
 
     public static function loadViewedPages()
     {
-        if (!empty($_COOKIE['jrv_pages'])) {
+        if (!empty($_COOKIE['jrv_pages_dated'])) {
             try {
-                self::$pageList = json_decode($_COOKIE['jrv_pages']);
+                self::$pageList = json_decode($_COOKIE['jrv_pages_dated']);
             } catch (Exception $e) {
                 print_r($e);
 
@@ -30,14 +30,26 @@ class jrv_lastviewed_repo
         if (!empty($post_id)) {
             self::loadViewedPages();
 
-            self::$pageList[] = $post_id;
-            self::$pageList = array_unique(self::$pageList);
+            // add new page and reverse order so the first occurance of a page is the last occurance found
+            self::$pageList[] = [$post_id, time()];
+            self::$Pagelist = array_reverse(self::$pagelist);
+            
+            // filter out all the duplicate page id's
+            self::$pageList = array_filter(self::$pageList, function($page, $index) use (self::$pagelist) {
+                return array_search($page[0], array_column(0, self::$pagelist)) === $index;
+            });
+            
+            // sort the visits by date
+            usort(self::$pagelist, function($a, $b) { 
+                return $a[1] > $b[1] ? 1 : $a[1] < $b[1] ? -1 : 0;
+            });
 
             // store a maximum of 25 pages
             while (count(self::$pageList) > 25) {
                 array_shift(self::$pageList);
             }
 
+            // save the cookie
             setcookie('jrv_pages', json_encode(self::$pageList), 0, COOKIEPATH, COOKIE_DOMAIN);
         }
     }
